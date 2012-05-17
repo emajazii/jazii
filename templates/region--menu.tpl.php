a60b3c15715ec54c9a908cbea5fb2bcbf975533f
Emana Imona <emajazii@yahoo.com>
2012-05-17 15:20:39 +0100

    initial commit for jazii theme

---
+++ templates/region--menu.tpl.php
@@ -0,0 +1,14 @@
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <?php if ($main_menu || $secondary_menu): ?>
+    <nav class="navigation">
+      <?php if (!module_exists('nice_menus')): ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+      <?php endif; ?>
+      
+      <?php //print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+    <?php endif; ?>
+    <?php print $content; ?>
+    </nav>
+  </div>
+</div>
