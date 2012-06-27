6e7ea1eea6feda6754f7afd8129d312a8d9f8762
Emana Imona <emajazii@yahoo.com>
2012-06-27 12:05:07 +0100

    fixed nice menu for theme

--- templates/region--menu.tpl.php
+++ templates/region--menu.tpl.php
@@ -6,6 +6,10 @@
         <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
       <?php endif; ?>
       
+      <?php if (module_exists('nice_menus')): ?>
+        <?php print theme('nice_menus_main_menu', array('direction' => 'down', 'dept' => 1)); ?>
+      <?php endif; ?>
+      
       <?php //print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
     <?php endif; ?>
     <?php print $content; ?>
